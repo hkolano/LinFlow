c222c0976e763cc9de96fd0fc49457e019d52051
Hannah Kolano <hkolano@olin.edu>
2017-12-07 14:10:11 -0500

    some example code

--- CylinderModel.m
+++ CylinderModel.m
@@ -1,28 +1,26 @@
+clf
 R=50;
 plot(0,0,'o', 'MarkerSize', 2*R);
 hold on;
 
 V=10;
-[x,y] = meshgrid(-0.01:0.005:0.01,-0.01:0.005:0.01);
+[x,y] = meshgrid(-1:.25:1,-1:.25:1);
 % Convert to Cartesian
 % x = r.*cos(t);
 % y = r.*sin(t);
 
-
 r=sqrt(x.^2+y.^2);
 t=atan2(y,x);
 
-
-Vr=V.*cos(t).*(1-R.^2./r.^2);
-Vt=-V.*sin(t).*(1+R.^2./r.^2);
+Vr=V.*cos(t).*(1-(R^2./r.^2));
+Vt=-V.*sin(t).*(1+(R^2./r.^2));
 
 u=Vr.*cos(Vt);
 v=Vr.*sin(Vt);
 
-
 quiver(x,y,u,v);
 
 hold on;
-startx=-0.015:0.002:0.015;
-starty=zeros(size(startx));
+starty=-1:.25:1;
+startx=[-1, -1, -1, -1, -1, -1, -1, -1, -1];
 streamline(x,y,u,v,startx,starty);
