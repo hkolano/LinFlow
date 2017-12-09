82183ff120d0ade90b6af27e7886cc376833a566
Evan Cusato <ecusato@olin.edu>
2017-12-09 13:45:06 -0500

    I give up...for now.

--- CircleExample.m
+++ CircleExample.m
@@ -7,6 +7,17 @@ for n = 1:length(y)
 xx(m,n) = x(m); yy(m,n) = y(n);
 psis(m,n) = V * y(n) - mu * y(n)/(x(m)^2+(y(n)+.01)^2) ...
 - (gam/4/pi)*log(x(m)^2+(y(n)+.01)^2);
+phis(m,n)= V * x(m) + mu * y(n)/(x(m)^2+(y(n)+.01)^2) ...
++ (gam/2/pi)*atan2(y(n),x(m));
+
+[dx, dy] = gradient(phis,0.2,0.2);
 end
 end
-contour(xx,yy,psis,[-3:.3:3],'k'), axis image
+%contour(xx,yy,psis,[-3:.3:3],'k'), axis image, hold on
+%contour(xx,yy,grad,[-3:.3:3],'r'), axis image
+starty = [-1:.1:1]
+startx = -1.*ones(size(starty))
+quiver(xx, yy, dx, dy)
+streamline(xx, yy, dx, dy,startx, starty)
+
+
