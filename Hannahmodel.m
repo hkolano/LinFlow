234aad73afc81bd26e20b9ffdfbf7b4a2a74bd9b
Hannah Kolano <hkolano@olin.edu>
2017-12-10 12:39:57 -0500

    doesn't work but has a better idea

--- Hannahmodel.m
+++ Hannahmodel.m
@@ -6,22 +6,31 @@ tea = (n^2-1)/3;
 rot = exp(j*a);
 
 
-R = 1 + e;
+R = 2 + e;
 theta = 0:pi/100:2*pi;  
 y = R * sin(theta);     
 x = R * cos(theta);
 
 for m = 1:length(x)
-    for n = 1:length(y)      
-        u = 1+(y(n)^2-x(m)^2)/((y(n)^2+x(m)^2).^2) - (gam/2/pi)*y(n)/(y(n)^2+x(m)^2);
-        v = -(2*x(m)*y(n))/(y(n)^2+x(m)^2) + (gam/2/pi)*x(m)/(y(n)^2+x(m)^2);
-        z = (u - e) + j.*(v + f);
-        w = rot .*(z + tea*1./z);
-        uTRAN(m, n) = real(w); vTRAN(m, n) = imag(w);
+    for n = 1:length(y)
+        psis(m,n) = V * y(n) - mu * y(n)/(x(m)^2+(y(n)+.01)^2) ...
+            - (gam/4/pi)*log(x(m)^2+(y(n)+.01)^2);
+        phis(m,n) = V * x(m) + mu * x(m)/(x(m)^2+(y(n)+.01)^2) ...
+            + (gam/2/pi)*atan2(y(n), x(m));
+        func = phis(m,n) + j.*psis(m,n);
+        w = rot .*(func + tea*1./func);
+        PHI(m, n) = real(w); PSI(m, n) = imag(w);
+        
+         %z = (x(m) - e) + 1i.*(y(n) + f);
+         %zed = z + tea.*1./z; 
+         %xx(m,n) = real(zed); yy(m,n) = imag(zed);
         xx(m,n) = x(m); yy(m,n) = y(n);
     end
 end
-figure
-quiver(x, y, uTRAN, vTRAN)
-%streamline(xx,yy,uTRAN,vTRAN,[0:0:0],[-1:.1:1]), axis image
+contour(xx,yy,PHI,[-3:.3:3],'c'), axis image, hold on
+contour(xx,yy,PSI,[-3:.3:3],'k'), axis image
 %axis([-5 5 -5 5])
+%figure
+%quiver(x, y, uTRAN, vTRAN)
+%streamline(xx,yy,uTRAN,vTRAN,[0:0:0],[-1:.1:1]), axis image
+
